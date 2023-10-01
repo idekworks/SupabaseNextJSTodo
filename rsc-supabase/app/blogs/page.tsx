c1dd65a91d83362236d74abe04a0a9d365e1e08a
idekworks <99700493+idekworks@users.noreply.github.com>
2023-10-01 10:59:00 +0900

    blog-list

---
+++ rsc-supabase/app/blogs/page.tsx
@@ -0,0 +1,13 @@
+import RouterBtn from "../components/route-btn";
+
+export default function BlogPage() {
+    return (
+        <div className="m-10 text-center">
+            <span className="text-lg">
+                <div className="my-5 flex justify-center">
+                    <RouterBtn />
+                </div>
+            </span>
+        </div>
+    )
+}
